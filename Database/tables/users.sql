8f1e90178d39f63dbea3d7233c2fee62e862427f
FYUDerma <a.zakaria.pro@gmail.com>
2025-03-12 01:05:55 +0100

    Refactor database initialization by replacing init.sql with modular SQL files and updating Dockerfile to copy all SQL scripts

--- Database/tables/users.sql
+++ Database/tables/users.sql
@@ -1,8 +1,8 @@
-CREATE TABLE "users" (
-    "userId" SERIAL PRIMARY KEY,
-    "username" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "isAdmin" BOOLEAN DEFAULT FALSE,
-    "carrotCount" INTEGER DEFAULT 0
+CREATE TABLE Users (
+  userID SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  passwords VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  carot INT NOT NULL,
+  isAdmin BOOLEAN
 );
