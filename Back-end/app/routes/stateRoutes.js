65bb632260324b1181c9604e5dcb45255434a0e6
FYUDerma <a.zakaria.pro@gmail.com>
2025-03-19 03:29:51 +0100

    Add state routes for saving and loading game progress

---
+++ Back-end/app/routes/stateRoutes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const User = require("../model/users");
+const GameState = require("../model/gameState");
+const authenticateToken = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Save progress
+router.post('/save', authenticateToken, async (req, res) => {
+  try {
+    const { username, clicks, upgrades } = req.body;
+
+    // Find the user
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Find or Create the game state
+    const [gameState, created] = await GameState.findOrCreate({ 
+      where: { userid: user.userid },
+      defaults: { clicks, upgrades }
+    });
+
+    // Update the game state if it already exists
+    if (!created) {
+      gameState.clicks = clicks;
+      gameState.upgrades = upgrades;
+      gameState.lastSaved = new Date();
+      await gameState.save();
+    }
+
+    res.status(200).json({ message: 'Progress saved successfully' });
+  } catch (err) {
+    console.error('Error during save:', err);
+    res.status(400).json({ error: err });
+  }
+});
+
+// Load progress
+router.get("/load/:username", authenticateToken, async (req, res) => {
+  try {
+    // Find the user
+    const user = await User.findOne({ where: { username: req.params.username } });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    };
+
+    // Find the user's game progress
+    const gameState = await GameState.findOne({ where: { userid: user.userid } });
+    if (!gameState) {
+      return res.status(404).json({ error: 'Progress not found' });
+    };
+
+    res.status(200).json({ gameState });
+  } catch (err) {
+    console.error('Error during load:', err);
+    res.status(400).json({ error: err }); 
+  }
+});
+
+module.exports = router;
