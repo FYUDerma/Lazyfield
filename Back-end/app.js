a4ba919e390f5aee50d16b67e71d00252a2861ca
FYUDerma <a.zakaria.pro@gmail.com>
2025-03-19 03:29:46 +0100

    Add state routes for managing game progress

--- Back-end/app.js
+++ Back-end/app.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 // Routes
 const userRoutes = require('./app/routes/userRoutes');
+const stateRoutes = require('./app/routes/stateRoutes');
 
 const app = express();
 const port = 3000;
@@ -16,7 +17,11 @@ app.use(express.static(path.join(__dirname, "../Front-end")));
 app.use(express.json());
 
 // Use routes
+// Register and login routes
 app.use('/api/users', userRoutes);
+// Game progress routes
+app.use('/api/state', stateRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
